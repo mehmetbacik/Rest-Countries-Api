6e4cf5e1cd5ed2db48ffd5263ab0e0c6e6d5a9c1
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-03-18 23:15:27 +0300

    Updated
    Updated for Logo.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Logo } from "../Logo";
 import { ThemeSwitcher } from "../ThemeSwitcher";
 
 const Header: React.FC = () => {
   return (
     <header>
+      <Logo />
       <ThemeSwitcher />
     </header>
   );
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Logo: React.FC = () => {
+  return <span>Where in the world?</span>;
+};
+
+export default Logo;
