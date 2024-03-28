a5d409e1c6bb5cfd2c81bc9243c5cc4fe2fef36e
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-03-28 21:52:15 +0300

    Update
    Updated for ListItem.

--- src/components/CountryList/CountryListItem.tsx
+++ src/components/CountryList/CountryListItem.tsx
@@ -8,11 +8,11 @@ interface CountryListItemProps {
 
 const CountryListItem: React.FC<CountryListItemProps> = ({ country }) => {
     return (
-        <Link to={`/country/${country.alpha3Code}`} className="themeListItem">
+        <Link to={`/country/${country.alpha3Code}`} className="themeListItem shadow-md">
             <div className='themeListImage w-full h-60 rounded-t-lg'>
                 <img src={country.flag} alt={country.name} className="w-full h-full object-cover rounded-t-lg" />
             </div>
-            <div className='themeListContent'>
+            <div className='themeListContent rounded-b-lg p-7'>
                 <h2 className="country-name">{country.name}</h2>
                 <p className="country-population">Population: {country.population}</p>
                 <p className="country-region">Region: {country.region}</p>
