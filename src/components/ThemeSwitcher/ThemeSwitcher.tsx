498a8bae72a7d8e72f31939aba3a944655a0e362
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-03-28 21:32:18 +0300

    Update
    Updated for ThemeSwitcher.

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 const ThemeSwitcher: React.FC = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.body.classList.add(theme);
+    return () => {
+      document.body.classList.remove(theme);
+    };
+  }, [theme]);
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    document.body.classList.remove(theme === "dark" ? "dark" : "light");
-    document.body.classList.add(newTheme);
   };
 
   return (
