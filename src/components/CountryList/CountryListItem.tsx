2ec0cffbeb876f3e1375cc36dbc0664b34c130b7
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-03-29 23:28:37 +0300

    Update
    Updated for CountryListItem.

--- src/components/CountryList/CountryListItem.tsx
+++ src/components/CountryList/CountryListItem.tsx
@@ -6,14 +6,18 @@ interface CountryListItemProps {
     country: Country;
 }
 
+const MAX_CHARACTERS = 25;
+
 const CountryListItem: React.FC<CountryListItemProps> = ({ country }) => {
+    const trimmedName = country.name.length > MAX_CHARACTERS ? `${country.name.substring(0, MAX_CHARACTERS)}...` : country.name;
+
     return (
         <Link to={`/country/${country.alpha3Code}`} className="themeListItem shadow-md">
             <div className='themeListImage w-full h-60 rounded-t-lg'>
                 <img src={country.flag} alt={country.name} className="w-full h-full object-cover rounded-t-lg" />
             </div>
             <div className='themeListContent rounded-b-lg p-7'>
-                <h2 className="country-name font-bold text-[25px] mb-3">{country.name}</h2>
+                <h2 className="country-name font-bold text-[25px] mb-3" title={country.name}>{trimmedName}</h2>
                 <div className="country-population flex items-center gap-1 mb-1">
                     <span className="font-bold">Population:</span> 
                     <p>{country.population}</p>
