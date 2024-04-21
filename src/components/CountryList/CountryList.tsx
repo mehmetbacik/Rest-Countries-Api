62c5bc2ae6d3a1ca331ff05340cf9b4d3bee3829
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2024-04-21 22:22:18 +0300

    Update
    Updated for List.

--- src/components/CountryList/CountryList.tsx
+++ src/components/CountryList/CountryList.tsx
@@ -8,7 +8,7 @@ interface CountryListProps {
 
 const CountryList: React.FC<CountryListProps> = ({ countries }) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[40px] md:gap-[80px]">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[40px] md:gap-[50px] lg:gap-[60px] xl:gap-[70px]">
             {countries.map(country => (
                 <CountryListItem key={country.alpha3Code} country={country} />
             ))}
